f1c86786e19cdbb3a2113a664dd21061921f4c11
hieunhhe17146 <hieunhhe171464@fpt.edu.vn>
2024-10-28 11:27:36 +0700

    coding favorite

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.play.services.maps)
+    implementation(libs.navigation.runtime)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
